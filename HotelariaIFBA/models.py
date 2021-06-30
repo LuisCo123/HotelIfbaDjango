23df40e6c194692856eae8640013d78536facc91
Luis Silva <luis.fernando.coelho14@gmail.com>
2021-06-29 22:21:03 -0300

    correct bug on foto field

--- HotelariaIFBA/models.py
+++ HotelariaIFBA/models.py
@@ -70,8 +70,8 @@ class Alojamento(models.Model):
     numero = models.IntegerField()
     capacidade = models.IntegerField()
     status = models.CharField( max_length=30, choices=TYPE_STATUS_ALOJAMENTO)
-    diaria = models.IntegerField()
-    foto = models.FileField(blank=True, null=True)
+    diaria = models.IntegerField(blank=True, null=True)
+    foto = models.FileField(blank=True, null=True, upload_to="HotelariaIFBA/roomImages/")
     def __str__(self):
         return "%s"%self.numero
 
